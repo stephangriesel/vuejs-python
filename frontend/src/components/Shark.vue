62187c01c12a79d65be690714fa7cde14b3cf358
Stephan Griesel <sgriesel@gmail.com>
2023-11-03 07:09:03 +0100

    new component

---
+++ frontend/src/components/Shark.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SharkyShark",
+  data() {
+    return {
+      msg: "Hello!",
+    };
+  },
+};
+</script>
